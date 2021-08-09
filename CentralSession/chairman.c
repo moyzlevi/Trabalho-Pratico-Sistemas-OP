5664353d1401923624e3f0e50ac1c7a4592617ab
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-09 11:32:15 -0300

    Initiated chairmain.c

--- CentralSession/chairman.c
+++ CentralSession/chairman.c
@@ -1,7 +1,47 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include<string.h>  
+#define SIZE_USERS 20
+
+struct message_buffer
+{
+    long msgtyp;
+    char arg[5];
+    char txt[100];
+};
+
+struct user
+{
+    long pid;
+    char user[20]; 
+};
+
+void handle_msgs(struct message_buffer msg){
+    if(strcmp(msg.arg,"login")==0){
+        for (int i = 0; i < SIZE_USERS; i++)
+        {
+            
+        }
+        
+    }
+}
 
 
 int main(int argc, char const *argv[])
-{
-    /* code */
+{   
+    struct message_buffer message;
+    struct user users[SIZE_USERS];
+    key_t main_key;
+    main_key = ftok("progfile", 65);
+    int id_fila = msgget(main_key, 0666 | IPC_CREAT);
+    
+    while (1)
+    {
+        msgrcv(id_fila, &message, sizeof(message), 1, 0);
+        handle_msgs(message);
+    }
+    
+    
     return 0;
 }
--- CentralSession/speaker.c
+++ CentralSession/speaker.c
@@ -1,6 +1,3 @@
-
-
-
 int main(int argc, char const *argv[])
 {
     /* code */
