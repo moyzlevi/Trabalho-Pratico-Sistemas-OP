80d5443c0ea1a6d56afa7eaf3f7c4887f2834663
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-28 20:51:25 -0300

    started shared memoery

--- PeerTalk/peerTalk.c
+++ PeerTalk/peerTalk.c
@@ -1,5 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024 /* make it a 1K shared memory segment */
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    key_t key;
+    int shmid;
+    char *data;
+    int mode;
+
+    if ((key = ftok("data.txt", 'R')) == -1) /*Here the file must exist */
+    {
+        perror("ftok");
+        exit(1);
+    }
+
+    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    data = shmat(shmid, NULL, 0);
+    if (data == (char *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+        printf("writing to segment: \"%s\"\n", "jose");
+        strncpy(data, "jose", SHM_SIZE);
+
+    
+        printf("segment contains: \"%s\"\n", data);
+
+    if (shmdt(data) == -1)
+    {
+        perror("shmdt");
+        exit(1);
+    }
+
     return 0;
 }
